d24c2b705499eb2388a40a6d91e7a1ed3b1fc55f
Kevin <liukaiwen-cn@outlook.com>
2020-07-13 13:12:48 +0800

    🎉 INT COMMIT:

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue'
 import './plugins/element.js'
 import  './assets/icon/iconfont.js'
 Vue.config.productionTip = false
-
+// init commit
 new Vue({
   render: h => h(App),
 }).$mount('#app')
