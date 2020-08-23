03e4672c0f662a8d84b08a2bae5fba906d8feceb
Kevin <liukaiwen-cn@outlook.com>
2020-08-23 18:15:05 +0800

    2020082301814460提交更新

--- autopush.bat
+++ autopush.bat
@@ -1,6 +1,22 @@
 git add .
-set timeStamp=%date:/=-%_%time%
-git commit -m  "%timeStamp%"
+    set yyyy=%date:~,4%
+    set mm=%date:~5,2%
+    set day=%date:~8,2% 
+    set "YYYYmmdd=%yyyy%%mm%%day%"
+    set "YYYYmmdd=%YYYYmmdd: =0%"
+    echo "YYYYmmdd%YYYYmmdd%YYYYmmdd"
+    set hh=%time:~0,2%
+    set mi=%time:~3,2%
+    set ss=%time:~6,2% 
+    set "hhmiss=%hh%%mi%%ss%"
+    set "hhmiss=%hhmiss: =0%"
+    echo "hhmiss%Time%hhmiss"
+    echo %hhmiss%
+    set "hhmiss=%hhmiss::=0%"
+    set "hhmiss=%hhmiss: =0%"
+    set "timestamp=%YYYYmmdd%%hhmiss%"
+set /p input=更新：
+git commit -m  "%timeStamp%%input%"
 git push
 
 exit
